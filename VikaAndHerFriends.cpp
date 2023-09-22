cdd88c193e101892899ceaf4ab1988e27938247d
Suvra Barua <99785331+ClassLessSB@users.noreply.github.com>
2023-09-22 20:48:54 +0600

    Add files via upload

---
+++ VikaAndHerFriends.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void vika(){
+    int n, m, k, x, y, sum = 0, answer = 0;
+    cin >> n >> m >> k >> x >> y;
+    
+    sum = (x+y)%2;
+    
+    int arr[k][2];
+    for(int i = 0; i<k; i++){
+        cin >> arr[i][0] >> arr[i][1];
+        int temp = ((arr[i][0]+arr[i][1])%2);
+        if(sum == temp){
+            answer = 1;
+        }
+    }
+    if(answer){
+        cout << "No" << endl;
+    }
+    else{
+        cout << "Yes" << endl;
+    }
+    
+    return;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    
+    for(int i = 0; i<t; i++){
+        vika();
+    }
+
+    return 0;
+}
