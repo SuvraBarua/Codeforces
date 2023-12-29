804c61228a12e87e93c26eb63966fc7d4498b83a
Suvra Barua <suvrabarua4@gmail.com>
2023-12-29 14:08:22 +0600

    Cf1913B

---
+++ SwapAndDelete.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class SwapAndDelete{
+    public static void swapAndDelete() {
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        int length = s.length();
+        int n0 = 0, n1 = 0;
+        
+        for(int i = 0; i<length; i++){
+            if(s.charAt(i) == '0') 
+                n0++ ;
+            else{
+                n1++;
+            }
+        }
+        
+        for(int i = 0; i<length; i++){
+            if(s.charAt(i) == '0' && n1 > 0){
+                n1--;
+            }
+            else if(s.charAt(i) == '1' && n0 > 0){
+                n0--;
+            }
+            else {
+                length = i;
+                break;
+            }
+        }
+        System.out.println(s.length() - length);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        
+        for(int i = 0; i<n; i++){
+            swapAndDelete();
+        }
+    }
+}
