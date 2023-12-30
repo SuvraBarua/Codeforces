3d572dc19d22eced451198109946ba428e23d911
Suvra Barua <suvrabarua4@gmail.com>
2023-12-30 18:37:37 +0600

    Update BrokenKeyboard.cpp

--- BrokenKeyboard.cpp
+++ BrokenKeyboard.cpp
@@ -6,25 +6,22 @@ void Acode(){
     string s;
     cin >> s;
     int bbc=0, sbc=0;
+    string ans = "";
     for(int i = s.length()-1; i>=0; i--){
         if(s[i] == 'b'){
             sbc++;
-            s.erase(i,1);
         }
         else if(s[i] == 'B'){
             bbc++;
-            s.erase(i, 1);
         }
         else if((s[i] > 64 && s[i] < 91) && bbc>0){
                bbc--;
-               s.erase(i, 1);
         }
         else if((s[i] > 96 && s[i] < 123) && sbc>0){
                sbc--;
-               s.erase(i, 1);
         }
         else{
-            continue;
+            ans = ans + s[i];
         }
     }
     cout << s << endl;
