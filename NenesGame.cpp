1ae5434faef199f14439d7d254b202afc3ef3a5f
Suvra Barua <suvrabarua4@gmail.com>
2024-04-13 20:50:36 +0600

    Create NenesGame.cpp

---
+++ NenesGame.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void sb(){
+    int k, q;
+    cin >> k >> q;
+    
+    int a[k];
+    int n[q];
+    
+    for(int i = 0; i<k; i++){
+        cin >> a[i];
+    }
+    for(int i = 0; i<q; i++){
+        cin >> n[i];
+    }
+    for(int i = 0; i<q; i++){
+        if(a[0]>n[i]){
+            cout << n[i] << " ";
+        }
+        else{
+            cout << (a[0]-1) << " ";
+        }
+    }
+    cout << "\n";
+}
+
+int main() {
+    
+    int t;
+    cin >> t;
+    
+    while(t){
+        sb();
+        t--;
+    }
+
+    return 0;
+}
