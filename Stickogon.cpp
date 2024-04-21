ebf47525cca29301508a81abb31fcc9c78e205a3
Suvra Barua <suvrabarua4@gmail.com>
2024-04-21 20:54:00 +0600

    Create Stickogon.cpp

---
+++ Stickogon.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isEqual(int a[], int b[], int s){
+    for(int i = 0; i<s; i++){
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+void sb(){
+    int n;
+    cin >> n;
+    
+    int a[n];
+    int counter[n] = {0};
+    
+    int ans = 0, tc = 0;
+    
+    for(int i = 0; i<n; i++){
+        cin >> a[i];
+    }
+    
+    for(int i = 0; i<n; i++){
+        if(counter[i] == 0){
+            for(int j = i+1; j<n;j++){
+                if(a[i] == a[j]){
+                    counter[j] = 1;
+                    tc++;
+                }
+            }
+        }
+        if(tc>2){
+            ans++;
+        }
+        tc=0;
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    for(int i = 0; i<n; i++){
+        sb();
+    }
+    return 0;
+}
