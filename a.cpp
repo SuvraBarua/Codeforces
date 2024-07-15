12b5ed58f1f58d992d23ede081266aa2d9bd2ccc
Suvra Barua <suvrabarua4@gmail.com>
2024-07-15 20:50:45 +0600

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void split(){
+    int n, k, ans = 0;
+    cin >> n >> k;
+    
+    if(n==1){
+        cout << 0 << endl;
+    }
+    
+    while(n>k){
+        n = n-(k-1);
+        ans++;
+    }
+    cout << ans+1 << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    
+    while(t){
+        split();
+        t--;
+    }
+}
