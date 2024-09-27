f086948e16f00a4534aa21b87b8cacafdad998f9
Suvra Barua <suvrabarua4@gmail.com>
2024-09-27 19:55:04 +0600

    Create MaxPlusSize.cpp

---
+++ MaxPlusSize.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void maxPlusSize(){
+    int n, max = 0, maxIndex = 0;
+    cin >> n;
+    int arr[n];
+    
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i = 0; i<n; i++){
+        if(max<arr[i]){
+            max = arr[i];
+            maxIndex = i;
+        }
+    }
+    if(n&1){
+        if(maxIndex&1){
+            cout << (n/2)+max;
+        }
+        else{
+            cout << (n/2)+max+1;
+        }
+    }
+    else{
+        cout << (n/2)+max;
+    }
+    cout << endl;
+}
+
+int main(){
+    int t; 
+    cin >> t;
+    while(t){
+        maxPlusSize();
+        t--;
+    }
+}
